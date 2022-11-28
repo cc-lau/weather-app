ebad0406e39ec18c736444c160fef4f81c1ff5c4
cc-lau <christianlau03@gmail.com>
2022-11-28 11:56:14 -0700

    Added built-in openWeather geolocation search feature to update weather

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 const searchButton = document.querySelector(".search-icon")
+const cityText = document.querySelector(".city-text")
 const temperatureText = document.querySelector(".temperature-text")
 const errorMessage = document.querySelector(".error-message")
+const feelsLikeText = document.querySelector(".feels-like-text")
+const windText = document.querySelector(".wind-text")
+const humidityText = document.querySelector(".humidity-text")
 searchButton.addEventListener('click', getWeather)
-
-
-
 async function getWeather() {
-/* 	const citySearch = document.querySelector(".city-search").value;
- */	
-  const citySearch = "London";
-  console.log(citySearch);
+	const citySearch = document.querySelector(".city-search").value;
 	try {
-		const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1135e03f79d5ce571fbfdce744ed1984&units=imperial", /* + citySearch, */ {
+		const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=1135e03f79d5ce571fbfdce744ed1984&units=imperial", {
 			mode: "cors"
 		});
 		const weatherData = await response.json();
 		console.log(weatherData)
+		//WEATHER DATA VARIABLES
+		const cityName = weatherData.name
 		const temperature = Math.round(weatherData.main.temp)
-
-		console.log(temperature)
+		const feelsLikeTemp = Math.round(weatherData.main.feels_like)
+		const windSpeed = Math.round(weatherData.wind.speed)
+		const humidity = weatherData.main.humidity
+		
+		//UPDATE TEXT
 		temperatureText.innerHTML = temperature + "&#8457;"
+		cityText.innerHTML = cityName
+		feelsLikeText.innerHTML = "FEELS LIKE: " + feelsLikeTemp + "&#8457;"
+		windText.innerHTML = "WIND: " + windSpeed + "MPH"
+		humidityText.innerHTML = "HUMIDITY: " + humidity + "%"
 	} catch (err) {
 		(errorMessage.innerHTML = "There was an error searching for this city"),
 		err;
